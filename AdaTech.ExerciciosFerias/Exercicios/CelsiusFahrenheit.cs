8fde7e091f2dd56aabce17750df59f90970ac49e
Luana <luhyxi@gmail.com>
2024-01-03 16:05:35 -0300

    Add project files.

---
+++ AdaTech.ExerciciosFerias/Exercicios/CelsiusFahrenheit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AdaTech.ExerciciosFerias.Utils;
+
+
+namespace AdaTech.ExerciciosFerias.Exercicios
+{
+    public class ConversorFahrenheitCelsius : GeneralUtils
+    {
+        private static float CelsiusParaFahrenheit(float valor) =>
+            (valor * 1.8f + 32);
+        private static float FahrenheitParaCelsius(float valor) =>
+            ((valor - 32) / 1.8f);
+
+        private static bool EhFahrenheitParaCelsius()
+        {
+            do
+            {
+                string escolha = Console.ReadLine().ToLower();
+                if (escolha != "a" && escolha != "b")
+                {
+                    Console.WriteLine("Por favor, escolha uma das opções");
+                    continue;
+                }
+                Console.Clear();
+                return escolha == "a" ? true : false;
+            }
+            while (true);
+        }
+
+
+        public static void DoCalculations()
+        {
+            Console.WriteLine("Você deseja converter de 'Fahrenheit para Celsius'(Pressione 'a') ou de 'Celsius Para Fahrenheit' ('Pressione 'b')?");
+            
+            bool escolha = EhFahrenheitParaCelsius();
+
+            float valor = ReceberValor<float>("float");
+
+            if (escolha) 
+            {
+                Console.WriteLine($"O resultado de Fº {valor} para Cº:{FahrenheitParaCelsius(valor)}");
+            }
+            else
+            {
+                Console.WriteLine($"O resultado de Cº {valor} Para Fº: {CelsiusParaFahrenheit(valor)}");
+            }
+            
+
+        }
+    }
+}
---
+++ AdaTech.ExerciciosFerias/Utils/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AdaTech.ExerciciosFerias.Utils
+{
+    public class GeneralUtils
+    {
+        public static T ReceberValor<T>(string NomeDoTipo)
+        {
+            var tipoEsperado = typeof(T);
+
+            do
+            {
+                Console.WriteLine($"Por favor, insira um valor do tipo {NomeDoTipo}:");
+                var input = Console.ReadLine();
+
+                try
+                {
+                    T valor = (T)Convert.ChangeType(input, tipoEsperado);
+                    Console.Clear();
+                    return valor;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Erro: Valor inserido não é do tipo {NomeDoTipo}.");
+                }
+                catch (InvalidCastException)
+                {
+                    Console.WriteLine($"Erro: Não é possível converter para o tipo {NomeDoTipo}.");
+                }
+
+            } while (true);
+        }
+
+    }
+}
